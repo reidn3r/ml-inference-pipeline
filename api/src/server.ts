f190cf15d126c4c77af94513929b7c73f06db2c6
reidn3r <rdn.adn00@gmail.com>
2026-01-06 23:33:51 -0300

    refactor(api): default api host

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,8 +2,12 @@ import 'dotenv/config'
 import { app } from "./app";
 
 const PORT: string = process.env.PORT
+const HOST: string = process.env.HOST || '0.0.0.0'; 
 
-app.listen({ port: Number(PORT) })
+app.listen({ 
+  port: Number(PORT),
+  host: HOST
+})
   .then(() => {
     console.log("[API]: Available")
     console.log(`[API]: ${PORT}`)
